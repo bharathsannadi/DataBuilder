ba84ab50c6de27f53b56a972e69f4dbfd63b232d
Bill Schofield <bill@billschofield.net>
2014-03-12 10:41:22 -0500

    Initial commit

---
+++ src/main/java/com/twu/Main.java
@@ -0,0 +1,8 @@
+package com.twu;
+
+public class Main {
+    public static void main(String[] args) {
+        // Your code goes here!
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/test/java/com/twu/MyTest.java
@@ -0,0 +1,16 @@
+package com.twu;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * This is a test sample. Delete this test and add your own.
+ */
+public class MyTest {
+    @Test
+    public void shouldNotWork() throws Exception {
+        assertThat(true, is(true));
+    }
+}
